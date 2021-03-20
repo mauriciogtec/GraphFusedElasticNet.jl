4722ea9c6b285281c21b80998ca9737159a62f68
Mauricio Tec <mauriciogtec@gmail.com>
2021-03-20 16:09:12 -0500

    added adaptive renormalization to gp

--- src/BayesianOpt.jl
+++ src/BayesianOpt.jl
@@ -1,6 +1,7 @@
 using Distributions
 using LinearAlgebra
 using Distances
+using Statistics
 
 ##
 
@@ -125,17 +126,19 @@ mutable struct RandomGaussianProcessSampler
     σ::Float64
     b::Float64
     offset::Float64
+    adaptive_normalization::Bool
 
     function RandomGaussianProcessSampler(
         dists::Vector{T};
         σ::Float64=0.001,
         a::Float64=0.5,
         b::Float64=1.0,
-        offset::Float64=0.0
+        offset::Float64=0.0,
+        adaptive_normalization::Bool = false
     ) where T <: Distribution
         X = Vector{Float64}[]
         y = Float64[]
-        new(dists, X, y, a, σ, b, offset)
+        new(dists, X, y, a, σ, b, offset, adaptive_normalization)
     end
 end
 Base.length(gp::RandomGaussianProcessSampler) = length(gp.X)
@@ -166,6 +169,12 @@ function gpsample(
         band = zeros(n)
         return pars, pred, band
     else
+        y = gp.y
+        if gp.adaptive_normalization
+            μ_seen = mean(y)
+            σ_seen = std(y)
+            y = (y .- μ_seen) ./ (σ_seen + eps())
+        end
         Xoldmat = hcat([x for x in gp.X]...)
         Xnewmat = vcat([x' for x in candidates]...)
         K00 = gp.b * rbfkernel(Xoldmat, gp.a)
@@ -177,7 +186,7 @@ function gpsample(
         #
         σ2 = gp.σ^2
         A = K00 + (σ2 + 1e-12)*I
-        β = gp.y .- offset
+        β = y .- offset
         pred = K10 * (A \ β) .+ offset
         Σ = K11 - K10 * (A \ K01) 
         Σ = Symmetric(Σ + 1e-12I)
@@ -187,6 +196,10 @@ function gpsample(
         pred = pred[order]
         band = band[order]
         Xnewmat = Xnewmat[:, order]
+        if gp.adaptive_normalization
+            pred = (σ_seen + eps()) .* pred .+ μ_seen
+            band = (σ_seen + eps()) .* band
+        end
         return Xnewmat, pred, band
     end
 end
@@ -214,6 +227,11 @@ function gpeval(gp::RandomGaussianProcessSampler)
     # gp regression mean and variance
     @assert length(gp) > 0  "add data first"
     y = gp.y
+    if gp.adaptive_normalization
+        μ_seen = mean(y)
+        σ_seen = std(y)
+        y = (y .- μ_seen) ./ (σ_seen + eps())
+    end
     σ2 = gp.σ^2
     X = hcat([x for x in gp.X]...)
     (size(X, 2) ≥ 1_000) && warn("kernel matrix too large, try sampling less candidates")
@@ -227,6 +245,10 @@ function gpeval(gp::RandomGaussianProcessSampler)
     Σ = K - K * (A \ K) 
     Σ = Symmetric(Σ + 1e-12I)
     band = sqrt.(diag(Σ))
+    if gp.adaptive_normalization
+        f = (σ_seen + eps()) .* f .+ μ_seen
+        band = (σ_seen + eps()) .* band
+    end
     return f, band
 end
 
@@ -242,6 +264,12 @@ function gpeval(
         band = zeros(n)
         return pred, band
     else
+        y = gp.y
+        if gp.adaptive_normalization
+            μ_seen = mean(y)
+            σ_seen = std(y)
+            y = (y .- μ_seen) ./ (σ_seen + eps())
+        end
         σ2 = gp.σ^2
         Xoldmat = hcat([x for x in gp.X]...)
         Xnewmat = hcat([x for x in Xnew]...)
@@ -253,48 +281,54 @@ function gpeval(
         s = length(gp)
         #
         A = K00 + (σ2 + 1e-12)*I
-        β = gp.y .- offset
+        β = y .- offset
         pred = K10 * (A \ β) .+ offset
         Σ = K11 - K10 * (A \ K01) 
         Σ = Symmetric(Σ + 1e-12I)
         band = sqrt.(diag(Σ))
+        if gp.adaptive_normalization
+            pred = (σ_seen + eps()) .* pred .+ μ_seen
+            band = (σ_seen + eps()) .* band
+        end
         return pred, band
     end
 end
 
 
 ## test random gp 
-# using Plots
-# N = 190
-# a = 0.5
-# σ = 0.05
-# b = 1.0
-# x = collect(range(0., stop=2π, length=N))
-# y = zeros(N)
-# draw(x::Float64, σ::Float64) = sin(x) + σ * randn()
-# for i in 1:N
-#     y[i] = draw(x[i], σ)
-# end
-# ytruth = sin.(x)
-# dists = [Uniform(0.0, 2π)]
-# gp = RandomGaussianProcessSampler(dists, a=a, σ=σ, b=b)
+using Plots
+N = 4
+a = 0.5
+σ = 0.05
+b = 1.0 / √(2π) * 2a
+scale = 100.0
+offset = -10.0 / scale
+x = collect(range(0., stop=2π, length=N))
+y = zeros(N)
+draw(x::Float64, σ::Float64) = (sin(x) + σ * randn()) / scale + offset
+for i in 1:N
+    y[i] = draw(x[i], σ)
+end
+ytruth = sin.(x) ./ scale .+ offset
+dists = [Uniform(0.0, 2π)]
+gp = RandomGaussianProcessSampler(dists, a=a, σ=σ, b=b, adaptive_normalization=true)
 
-# for i in 1:N
-#     addobs!(gp, [x[i]], y[i])
-# end
+for i in 1:N
+    addobs!(gp, [x[i]], y[i])
+end
 
-# @time pars, pred, band = gpsample(gp, 16; batch_size=500)
-# x_sample = vec(pars)
-# y_sample = pred
+@time pars, pred, band = gpsample(gp, 16; batch_size=500)
+x_sample = vec(pars)
+y_sample = pred
 
-# xseq = [[xi] for xi in collect(range(0., stop=2π, length=50))]
-# xseq, pred_eval, band_eval = gpeval(gp, xseq)
+xseq = [[xi] for xi in collect(range(0., stop=2π, length=50))]
+pred_eval, band_eval = gpeval(gp, xseq)
+xseq = vcat(xseq...)
 
-
-# p1 = plot(x, ytruth, label="truth", linestyle=:dash, color="black")
-# plot!(p1, vec(xseq), pred_eval, ribbon=1.96 * band_eval, fillalpha=.2, label="fit_0", color="blue")
-# plot!(p1, x, gp.y, st = :scatter, label="candidate_0", color="blue", alpha=0.4)
-# plot!(p1, x_sample, y_sample, st = :scatter, label="candidate_1", color=:red)
+p1 = plot(x, ytruth, label="truth", linestyle=:dash, color="black")
+plot!(p1, vec(xseq), pred_eval, ribbon=1.96 * band_eval, fillalpha=.2, label="fit_0", color="blue")
+plot!(p1, x, gp.y, st = :scatter, label="candidate_0", color="blue", alpha=0.4)
+plot!(p1, x_sample, y_sample, st = :scatter, label="candidate_1", color=:red)
 # ylims!(p1, -2.5, 2.5)
 
 
